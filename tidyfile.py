6990992a964b685b4935e4244d8c07d8e45cfdee
xireiki <xiayinlily@outlook.com>
2021-08-22 02:41:37 +0800

    优化代码，修改提示

--- tidyfile.py
+++ tidyfile.py
@@ -6,71 +6,7 @@ import platform
 import requests
 import json5 as json
 
-def main():
-  parser = argparse.ArgumentParser(description="这是一个用于整理文件夹的python脚本")
-  parser.add_argument('-i', '--input', help="需要整理的文件夹", default=None, dest="i")
-  parser.add_argument('-o', '--output', help="整理后的文件保存位置", default=None, dest="o")
-  parser.add_argument('-v', '--version', help="显示版本信息", action="store_true", dest="v")
-  parser.add_argument('-u', '--update', help="更新脚本", action="store_true", dest="update")
-  parser.add_argument('-c', '--compulsion', help="强制执行，仅对更新脚本有效", action="store_true", dest="compulsion")
-  args = parser.parse_args();
-  
-  #版本信息
-  name="tidyfile";
-  author="哔哩哔哩@Gulanguage";
-  blog="https://github.com/xireiki/";
-  version_number=111;
-  version="1.0.11";
-  
-  if args.i:
-    if not args.i.endswith("/"):
-      path = args.i + "/";
-    else:
-      path = args.i;
-  elif args.v:
-    print("脚本名称："+name);
-    print("脚本作者："+author);
-    print("我的主页："+blog);
-    print("当前版本："+version);
-    sys.exit(0);
-  elif args.update:
-    print("正在检查更新...",end="");
-    update = requests.get("https://tidyfile.xireiki.com/update.json");
-    ujtext = json.loads(update.text);
-    def update():
-      tidyfile = requests.get(ujtext["version_info"]["update_url"]);
-      update_tidyfile = open(sys.argv[0], "w");
-      update_tidyfile.write(tidyfile.text);
-      update_tidyfile.close();
-      print("更新完成！");
-    if ujtext["version_info"]["version_number"] > version_number:
-      print("发现新版本："+name+" v"+ujtext["version_info"]["version"]);
-      print("更新内容：\n"+ujtext["version_info"]["version_text"]);
-      tidyfile_update_confirm = input("是否安装 \""+name+" v"+ujtext["version_info"]["version"]+"\"? [Y/n] ");
-      if tidyfile_update_confirm != "n" and tidyfile_update_confirm != "N" and tidyfile_update_confirm != "no" and tidyfile_update_confirm != "NO" and tidyfile_update_confirm != "No":
-        update();
-    elif args.compulsion:
-      print("您选择了强制更新\n正在为您安装...",end="");
-      update();
-    else:
-      print("当前已是最新版本！");
-    sys.exit(0);
-  else:
-    if platform.system() == "Linux":
-      print("请输入参数，使用 \"" + os.path.basename(__file__) + " --help\" 查看帮助");
-      path = None;
-      sys.exit(0);
-    elif platform.system() == "Windows":
-      path = "./";
-  
-  if args.o:
-    if not args.o.endswith("/"):
-      outpath = args.o + "/";
-    else:
-      outpath = args.o;
-  else:
-    outpath = path;
-  
+def clear(path, outpath):
   folders = ["video","audio","document","backup","picture","program","others"];
   program_folder = ["c","cpp","python","java","lua","php","shell"];
   other_folder = ["zippack"];
@@ -160,6 +96,73 @@ def main():
     if file_name != "video" and file_name != "audio" and file_name != "document" and file_name != "backup" and file_name != "picture" and file_name != "program" and file_name != "others" :
       print(path + file_name + " >>> " + outpath + "others/" + file_name)
       os.rename(path + file_name,outpath + "others/" + file_name)
+
+def main():
+  parser = argparse.ArgumentParser(description="这是一个用于整理文件夹的python脚本")
+  parser.add_argument('-i', '--input', help="需要整理的文件夹", default=None, dest="i")
+  parser.add_argument('-o', '--output', help="整理后的文件保存位置", default=None, dest="o")
+  parser.add_argument('-v', '--version', help="显示版本信息", action="store_true", dest="v")
+  parser.add_argument('-u', '--update', help="更新脚本", action="store_true", dest="update")
+  parser.add_argument('-c', '--compulsion', help="强制执行，仅对更新脚本有效", action="store_true", dest="compulsion")
+  args = parser.parse_args();
+  
+  #版本信息
+  name = "tidyfile";
+  author = "Github@xireiki";
+  blog = "https://github.com/xireiki";
+  version_number = 111;
+  version = "1.0.11";
+  updateUrl = "https://tidyfile.xireiki.com/update.json";
+  
+  if args.i:
+    if not args.i.endswith("/"):
+      path = args.i + "/";
+    else:
+      path = args.i;
+  elif args.v:
+    print("脚本名称：" + name);
+    print("脚本作者：" + author);
+    print("我的主页：" + blog);
+    print("当前版本：" + version);
+    print("下载地址：" + updateUrl);
+    sys.exit(0);
+  elif args.update:
+    print("正在检查更新...");
+    update = requests.get(updateUrl);
+    ujtext = json.loads(update.text);
+    def update():
+      tidyfile = requests.get(ujtext["version_info"]["update_url"]);
+      with open(sys.argv[0], "w") as _:
+        _.write(tidyfile.text);
+      print("更新完成！");
+    if ujtext["version_info"]["version_number"] > version_number:
+      print("发现新版本："+name+" v"+ujtext["version_info"]["version"]);
+      print("更新内容：\n"+ujtext["version_info"]["version_text"]);
+      tidyfile_update_confirm = input("是否安装 \""+name+" v"+ujtext["version_info"]["version"]+"\"? [Y/n] ");
+      if tidyfile_update_confirm != "n" and tidyfile_update_confirm != "N" and tidyfile_update_confirm != "no" and tidyfile_update_confirm != "NO" and tidyfile_update_confirm != "No":
+        update();
+    elif args.compulsion:
+      print("您选择了强制更新\n正在为您安装...",end="");
+      update();
+    else:
+      print("当前已是最新版本！");
+    sys.exit(0);
+  else:
+    if platform.system() == "Linux":
+      print("请输入参数，使用 \"" + os.path.basename(__file__) + " --help\" 查看帮助");
+      path = None;
+      sys.exit(0);
+    elif platform.system() == "Windows":
+      path = "./";
+  
+  if args.o:
+    if not args.o.endswith("/"):
+      outpath = args.o + "/";
+    else:
+      outpath = args.o;
+    clear(path, output);
+  else:
+    clear(path, path);
 
 def cbm_main(path, filename, name):
   main();
